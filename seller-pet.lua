ac697394b8991b1fa58550803ee56d0ad714982a
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-10-18 19:45:23 -0300

    Update seller-pet.lua

--- seller-pet.lua
+++ seller-pet.lua
@@ -3,45 +3,59 @@ getgenv().Settings = {
     Sniper = {
         Active = false,
         Items = {
-            --// Example Settings, everything is editable.
-            ["All Rarity: Legendary"] = {Class = "Pet", Price = "0%"}, --// Normal RAP.
-            ["All Rarity: Exotic"] = {Class = "Pet", Price = "20%"}, --// 20% below or less of RAP.
-            ["All Rarity: Mythical"] = {Class = "Pet", Price = "+20%"}, --// 20% above or less of RAP.
-            ["RAP Above: 500k"] = {Class = "Pet", Price = "15%", UseCosmicValues = true}, --// 15% below Cosmic Value price & RAP Above 500k
-            ["Difficulty Above: 5m"] = {Class = "Pet", Price = "+50%", UseCosmicValues = true}, --// 50% above Cosmic Value price & Difficulty Above 5m
-            ["Kitsune Fox"] = {Class = "Pet", Price = 15000, InventoryLimit = 52}, --// Normal Listing Price.
+            --// Example Settings showcasing features.
+            SearchTerminal = {
+                [[ Terminal doesn't support custom keywords like "All Huges". ]],
+
+                ["Mini Chest"] = {Class = "Misc", Price = "2%"},
+                ["Coins 5"] = {Class = "Potion", Price = 1},
+                ["Shiny Apple"] = {Class = "Fruit", Price = "+5%"},
+            },
+
+            
+            ["All Huges"] = {Class = "Pet", Price = "80%", DetectManipulation = true, AllTypes = true},
+            ["Rainbow All Huges"] = {Class = "Pet", Price = 20000000},
+            ["All Titanics"] = {Class = "Pet", Price = "30%", UseCosmicValues = true, AllTypes = true},
+            ["All Items"] = {Class = "Misc", Price = "20%", AllTiers = true},
+            ["All Exclusives"] = {Class = "Pet", Price = 500000},
+            ["Huge Fancy Axolotl"] = {Class = "Pet", Price = "+10%"},
+            ["Lightning 1"] = {Class = "Charm", Price = "+2%"},
         },
         Serverhop = {
             ["Switch Servers"] = true,
             ["Teleport Delay (s)"] = 3,
-            ["Save # Last Joined Servers"] = 10,
+            ["Add Pro Plaza Lobbies"] = false,
+            ["Constant Terminal Searching"] = true,
+            ["Terminal Searches per Item"] = 3,
+            ["Save # Servers"] = 10,
         },
         Webhook = {
             ["URL"] = "https://discord.com/api/webhooks/1296593849296097372/i8wrmMNLTGmMBJT1_DszJa72x3WzMKn86Ni32AcjcoJrGhoGt54tZ9o9pPWc0lTjeO2w",
             ["Send Embeds"] = true,
             ["Remove Username"] = true,
+            ["Ping on Huges 'n Titanics"] = true,
             ["Global Snipes"] = true,
         },
         StopParams = {
             ["Limits Reached"] = false,
-            ["Diamonds Hit: 250k"] = false,
+            ["Diamonds Hit: 250k"] = true,
             ["60 Minutes"] = false,
-            ["Switch To Selling"] = false,
+            ["Switch To Selling"] = true,
         },
     },
+
     Seller = {
         Active = true,
         Items = {
-            --// Example Settings, everything is editable.
-            ["All Rarity: Superior"] = {Class = "Pet", Price = "0%"}, --// Normal RAP.
-            ["All Rarity: Exotic"] = {Class = "Pet", Price = "20%"}, --// 20% above or less of RAP.
-            ["All Rarity: Mythical"] = {Class = "Pet", Price = "-20%"}, --// 20% below or less of RAP.
-            ["RAP Above: 15m"] = {Class = "Pet", Price = -5000}, --// 5k off of RAP & RAP Above 15m.
-            ["Difficulty Above: 5m"] = {Class = "Pet", Price = "10%"}, --// 10% above or less of RAP & Difficulty Above 5m.
+            ["Huge Abyssal Jellyfish"] = {Class = "Pet", Price = 50000000},
+            ["Large Potion Bundle"] = {Class = "Misc", Price = "10%"},
+            ["All Huges"] = {Class = "Pet", Price = "-5%"},
+            ["All Items"] = {Class = "Lootbox", Price = -5000},
         },
         Serverhop = {
             ["Switch Servers"] = true,
             ["Teleport Delay (m)"] = 15,
+            ["Add Pro Plaza Lobbies"] = true,
         },
         Webhook = {
             ["URL"] = "https://discord.com/api/webhooks/1296593849296097372/i8wrmMNLTGmMBJT1_DszJa72x3WzMKn86Ni32AcjcoJrGhoGt54tZ9o9pPWc0lTjeO2w",
@@ -49,19 +63,18 @@ getgenv().Settings = {
             ["Remove Username"] = true,
         },
         StopParams = {
-            ["Item Runout"] = true,
+            ["Item Runout"] = false,
             ["Diamonds Hit: 1b"] = false,
             ["60 Minutes"] = false,
             ["Switch To Sniping"] = false,
         },
         Other = {
             ["Auto Accept Mail"] = true,
-            ["Diamonds Hit: 1b Sendout"] = "0wqrr79",
             ["Always Try Adding Listings"] = true,
-            ["Never Join Friendslist"] = false,
+            ["Never Join Friendslist"] = true,
         },
     },
     
     [[ Thank you for using System Exodus <3! ]]
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/717430e3bbde3530feb824de729fcc90.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/60a293774110e918789cddc0e20be048.lua"))()
