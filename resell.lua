e361b7c105b1208154f8cde2efb39856ff7f188f
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-10-30 11:56:51 -0300

    Update resell.lua

--- resell.lua
+++ resell.lua
@@ -5,8 +5,8 @@ getgenv().Settings = {
         Items = {
             --// Example Settings, everything is editable.
             ["All Rarity: Legendary"] = {Class = "Pet", Price = "20%"}, --// Normal RAP.
-            ["All Rarity: Exotic"] = {Class = "Pet", Price = "20%"}, --// 20% below or less of RAP.
-            ["All Rarity: Mythical"] = {Class = "Pet", Price = "20%"}, --// 20% above or less of RAP.
+            ["All Rarity: Exotic"] = {Class = "Pet", Price = "25%"}, --// 20% below or less of RAP.
+            ["All Rarity: Mythical"] = {Class = "Pet", Price = "30%"}, --// 20% above or less of RAP.
             ["RAP Above: 500k"] = {Class = "Pet", Price = "20%", UseCosmicValues = true}, --// 15% below Cosmic Value price & RAP Above 500k
 
         },
@@ -25,43 +25,40 @@ getgenv().Settings = {
             ["Limits Reached"] = false,
             ["Diamonds Hit: 250k"] = false,
             ["60 Minutes"] = false,
-            ["Switch To Selling"] = false,
+            ["Switch To Selling"] = true,
         },
     },
     Seller = {
-        Active = false,
+        Active = true,
         Items = {
             --// Example Settings, everything is editable.
-            ["All Rarity: Superior"] = {Class = "Pet", Price = "0%"}, --// Normal RAP.
-            ["All Rarity: Exotic"] = {Class = "Pet", Price = "20%"}, --// 20% above or less of RAP.
-            ["All Rarity: Mythical"] = {Class = "Pet", Price = "-20%"}, --// 20% below or less of RAP.
+            ["All Rarity: Superior"] = {Class = "Pet", Price = "-5%"}, --// Normal RAP.
+            ["All Rarity: Exotic"] = {Class = "Pet", Price = "-5%"}, --// 20% above or less of RAP.
+            ["All Rarity: Mythical"] = {Class = "Pet", Price = "-5%"}, --// 20% below or less of RAP.
             ["RAP Above: 15m"] = {Class = "Pet", Price = -5000}, --// 5k off of RAP & RAP Above 15m.
-            ["Difficulty Above: 5m"] = {Class = "Pet", Price = "10%"}, --// 10% above or less of RAP & Difficulty Above 5m.
-            ["Kitsune Fox"] = {Class = "Pet", Price = 15000, Amount = 2}, --// Normal Listing Price.
+            ["Difficulty Above: 5m"] = {Class = "Pet", Price = "-5%"}, --// 10% above or less of RAP & Difficulty Above 5m.
         },
         Serverhop = {
             ["Switch Servers"] = true,
             ["Teleport Delay (m)"] = 15,
         },
         Webhook = {
-            ["URL"] = "",
+            ["URL"] = "https://discord.com/api/webhooks/1296593849296097372/i8wrmMNLTGmMBJT1_DszJa72x3WzMKn86Ni32AcjcoJrGhoGt54tZ9o9pPWc0lTjeO2w",
             ["Send Embeds"] = true,
             ["Remove Username"] = true,
         },
         StopParams = {
-            ["Item Runout"] = true,
+            ["Item Runout"] = false,
             ["Diamonds Hit: 1b"] = false,
             ["60 Minutes"] = false,
-            ["Switch To Sniping"] = false,
+            ["Switch To Sniping"] = true,
         },
         Other = {
             ["Auto Accept Mail"] = true,
-            ["Diamonds Hit: 1b Sendout"] = "Jxnttt",
+            ["Diamonds Hit: 500k Sendout"] = "FASTZIN_STORE",
             ["Always Try Adding Listings"] = true,
             ["Never Join Friendslist"] = false,
         },
     },
-    
-    [[ Thank you for using System Exodus <3! ]]
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/717430e3bbde3530feb824de729fcc90.lua"))()
