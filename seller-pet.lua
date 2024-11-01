e099b594c4d2b6938f4aa748050f6318080636f2
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-11-01 18:49:36 -0300

    Update seller-pet.lua

--- seller-pet.lua
+++ seller-pet.lua
@@ -4,9 +4,9 @@ getgenv().Settings = {
         Active = false,
         Items = {
             --// Example Settings, everything is editable.
-            ["All Rarity: Legendary"] = {Class = "Pet", Price = "0%"}, --// Normal RAP.
-            ["All Rarity: Exotic"] = {Class = "Pet", Price = "0%"}, --// 20% below or less of RAP.
-            ["All Rarity: Mythical"] = {Class = "Pet", Price = "0%"}, --// 20% above or less of RAP.
+            ["All Rarity: Legendary"] = {Class = "Pet", Price = "10%"}, --// Normal RAP.
+            ["All Rarity: Exotic"] = {Class = "Pet", Price = "10%"}, --// 20% below or less of RAP.
+            ["All Rarity: Mythical"] = {Class = "Pet", Price = "10%"}, --// 20% above or less of RAP.
             ["RAP Above: 500k"] = {Class = "Pet", Price = "15%", UseCosmicValues = true}, --// 15% below Cosmic Value price & RAP Above 500k
             ["Difficulty Above: 5m"] = {Class = "Pet", Price = "+50%", UseCosmicValues = true}, --// 50% above Cosmic Value price & Difficulty Above 5m
             ["Kitsune Fox"] = {Class = "Pet", Price = 15000, InventoryLimit = 52}, --// Normal Listing Price.
