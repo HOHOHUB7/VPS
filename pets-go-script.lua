a700b84c2488dc03759290417214410ca764bb81
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-11-04 03:48:43 -0300

    Create pets-go-script.lua

---
+++ pets-go-script.lua
@@ -0,0 +1,17 @@
+script_key="dOfSrLWDyauFbCSkkEFAHvWlUvslwqrV";
+
+getgenv().petsGoConfig = {
+EVENT_EGG = true,
+    HIDE_NAME = false,  
+    WEBHOOK_URL = "https://discord.com/api/webhooks/1302786988113395772/yl6AVYbLqcx1ryFt1ebOT37TxbVdWZLD_r501cdfFNtV-xBN05zDWPQetTx5gM8G8ymm",
+    MAILING_WEBHOOK_URL = "https://discord.com/api/webhooks/1302786862032883783/B4cACiajxWLBE3OOHv6zPPZESuZ7dw6C7MX_uUYmvWgB_K_ew3ESQgI6Iov232r4Wlrr",
+    DISCORD_ID = "",
+    WEBHOOK_ODDS = 1000000, 
+    
+    MAIL_PET = true,  
+    MAIL_WEBHOOK_ODDS = 10000000, 
+    MAIL_PET_ODDS = 1000000,  
+    USERNAME_TO_MAIL = "petfastziinn01" 
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HOHOHUB7/VPS/refs/heads/main/pets-go-teck.lua"))()
