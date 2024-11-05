5b2e6c5fcde85abffab772ed426d1c7f3d52c03c
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-11-05 18:07:21 -0300

    Update pets-go-script.lua

--- pets-go-script.lua
+++ pets-go-script.lua
@@ -5,7 +5,7 @@ EVENT_EGG = true,
     HIDE_NAME = false,  
     WEBHOOK_URL = "https://discord.com/api/webhooks/1302786988113395772/yl6AVYbLqcx1ryFt1ebOT37TxbVdWZLD_r501cdfFNtV-xBN05zDWPQetTx5gM8G8ymm",
     MAILING_WEBHOOK_URL = "https://discord.com/api/webhooks/1302786862032883783/B4cACiajxWLBE3OOHv6zPPZESuZ7dw6C7MX_uUYmvWgB_K_ew3ESQgI6Iov232r4Wlrr",
-    DISCORD_ID = "",
+    DISCORD_ID = "1037147029614362624",
     WEBHOOK_ODDS = 1000000, 
     
     MAIL_PET = true,  
