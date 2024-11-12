5645d871a7df5223f22f6f95fe44ad7c3d851f3e
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-11-11 23:07:10 -0300

    Create fisch-normalhub.lua

---
+++ fisch-normalhub.lua
@@ -0,0 +1,20 @@
+_G.Enchant = true 
+_G.CustomUseRod = "" -- อย่าใส่ชื่อเบ็ตผิด
+_G.Kaitun = true
+    Config = {
+        ['Lock Fps'] = 30,
+        ['Farm Fish'] = true,
+        ['White Screen'] = false,
+        ['Black Screen'] = false,
+        ["WebHook"] = {
+            ["Link"] = "https://discord.com/api/webhooks/1305714991235731486/Vp9vt8wcZ9OSvDTZ53pFjI2oKyc6JT_Tu6vstMBPKJZsx4tZ1o352Vf_2z7JIh0PDC2_",
+            ["Discord Id"] = "",
+            ["Delay Sending"] = 30, 
+            ["Auto Sending"] = true,
+            ["Ping With Discord Id"] = false,
+        },
+    }
+_G.Key = "ALL-549QF-HEFFQ"
+_G.DiscordId = "1037147029614362624"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MonkiTeam/Nomal-Project/refs/heads/main/FischKaitun"))();
