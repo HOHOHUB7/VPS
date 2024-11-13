71943c8425ae406156e6698bb080a0c1f35f9bed
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-11-13 16:28:25 -0300

    Update kinglega.lua

--- kinglega.lua
+++ kinglega.lua
@@ -20,31 +20,3 @@ getgenv().Key = "MARU-1OAVS-9DJVD-9MD3-ESH8R-5IGZQ"
 getgenv().id = "1037147029614362624"
 getgenv().Script_Mode = "Kaitun_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
-break;
-
-
-
-
-_G.Key = "ALL-549QF-HEFFQ"
-
-_G.Enchant = true 
-_G.CustomUseRod = "" -- อย่าใส่ชื่อเบ็ตผิด ชื่อเต็ม
-_G.CustomBait = ""-- อย่าใส่ชื่อเหยื่อผิด ชื่อเต็ม
-_G.Kaitun = true
-    Config = {
-        ['Lock Fps'] = 30,
-        ['Farm Fish'] = true,
-        ['White Screen'] = false,
-        ['Black Screen'] = false,
-        ["WebHook"] = {
-            ["Link"] = "",
-            ["Discord Id"] = "",
-            ["Delay Sending"] = 30, 
-            ["Auto Sending"] = false,
-            ["Ping With Discord Id"] = false,
-        },
-    }
-
-_G.DiscordId = "1037147029614362624"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MonkiTeam/Nomal-Project/refs/heads/main/FischKaitun"))();
