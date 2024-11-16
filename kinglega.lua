a567d1976446587b145dc17de40ea4b017dd6e2c
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-11-16 18:08:49 -0300

    Update kinglega.lua

--- kinglega.lua
+++ kinglega.lua
@@ -1,50 +1,15 @@
--- If you have more than 1 key, select only 1.
-_G.Key = "ALL-549QF-HEFFQ"
-
-_G.DiscordId = "1037147029614362624"
-
-_G.Kaitun = true
-Config = {
-        ['Lock Fps'] = 30,
-        ['Farm Fish'] = true,
-        ['White Screen'] = false,
-        ['Black Screen'] = true,
-        ["Fully Auto All"] = true,
-        ["Enchant Lock"] = {
-            ["Rapid Rod"] = "Insight",
-            ["Trident Rod"] = "Hasty",    
-        },
-        ["Custom"] = {
-            ["Custom Use Rod"] = nil,
-            ["Custom Bait"] = nil,
-        },
-        ["AutoBuy"] = {
-            ["Fully Get Aurora Rod"] = false,
-        },
-        ["WebHook"] = {
-            ["Link"] = '',
-            ["Discord Id"] = '',
-            ["Delay Sending"] = 30, 
-            ["Auto Sending"] = false,
-            ["Ping With Discord Id"] = false,
-        },
-    }
-
-
-repeat task.wait() until game:IsLoaded() 
-getgenv().Hermanos_Settings = {
-	['key'] = 'caa42f4d-247f-4eeb-8204-92e4d6b705fd',
-	['PC'] = 'DDC-1',
+_G.Settings = {
+    ["AutoFishing"] = true,
+    ["SellFish"] = true,
+    ['MethodShake'] = "Fast", -- Fast, Fix bug
+    ["Instant_Coin"] = true,
+    ["SetSteek"] = 100,
+    ["FpsBoost"] = true,
+    ["Black_Screen"] = true
 }
-task.spawn(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-script/main/fisch-main.lua'))() end)
 
-print("[DEBUG] Script started")
-local success, result = pcall(function()
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/MonkiTeam/Nomal-Project/refs/heads/main/FischKaitun"))()
-end)
 
-if success then
-    print("[DEBUG] Script loaded successfully")
-else
-    warn("[DEBUG] Failed to load script:", result)
-end
+getgenv().Key = "MARU-CQ4OC-5XUTJ-HW6T-TO7FF-XYFP5"
+getgenv().id = "1037147029614362624"
+getgenv().Script_Mode = "Kaitun_Script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
