56b459b0dc366fea4845815170df15923fdb3d07
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-11-16 16:19:28 -0300

    Update kinglega.lua

--- kinglega.lua
+++ kinglega.lua
@@ -4,11 +4,16 @@ _G.Key = "ALL-549QF-HEFFQ"
 _G.DiscordId = "1037147029614362624"
 
 _G.Kaitun = true
-    Config = {
+Config = {
         ['Lock Fps'] = 30,
         ['Farm Fish'] = true,
         ['White Screen'] = false,
         ['Black Screen'] = true,
+        ["Fully Auto All"] = true,
+        ["Enchant Lock"] = {
+            ["Rapid Rod"] = "Insight",
+            ["Trident Rod"] = "Hasty",    
+        },
         ["Custom"] = {
             ["Custom Use Rod"] = nil,
             ["Custom Bait"] = nil,
@@ -16,11 +21,10 @@ _G.Kaitun = true
         ["AutoBuy"] = {
             ["Fully Get Aurora Rod"] = false,
         },
-        ["Fully Auto All"] = true,
         ["WebHook"] = {
-            ["Link"] = 'https://discord.com/api/webhooks/1305743894444773436/9VCZphRg4IlHsLR3C3alXwqNt713DYrSGvGN5owN7DmDi3Dm749NdbZeXchBc1bUBMRb',
+            ["Link"] = '',
             ["Discord Id"] = '',
-            ["Delay Sending"] = 600, 
+            ["Delay Sending"] = 30, 
             ["Auto Sending"] = false,
             ["Ping With Discord Id"] = false,
         },
