68533963f3027cc1b716921078bb50d9f8f93d01
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-11-16 06:31:05 -0300

    Update kinglega.lua

--- kinglega.lua
+++ kinglega.lua
@@ -1,22 +1,39 @@
-_G.Settings = {
-    ["AfkCheckTime"] = 300, -- Seconds
-    ["AutoFishing"] = true,
-    ["SellFish"] = {
-        ["Enable"] = true,
-        ["Method"] = {
-            ["Event"] = true,
-            ["Enchant"] = true,
-            ["Mythical"] = true,
-            ["Legenday"] = true
-        }
-    },
-    ['MethodShake'] = "Fast", -- Fast, Fix bug
-    ["Instant_Coin"] = true,
-    ["SetSteek"] = 100,
-    ["FpsBoost"] = true,
-    ["Black_Screen"] = true
-}
-getgenv().Key = "MARU-1OAVS-9DJVD-9MD3-ESH8R-5IGZQ"
-getgenv().id = "1037147029614362624"
-getgenv().Script_Mode = "Kaitun_Script"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
+-- If you have more than 1 key, select only 1.
+_G.Key = "ALL-549QF-HEFFQ"
+
+_G.DiscordId = "1037147029614362624"
+
+_G.Kaitun = true
+    Config = {
+        ['Lock Fps'] = 30,
+        ['Farm Fish'] = true,
+        ['White Screen'] = false,
+        ['Black Screen'] = true,
+        ["Custom"] = {
+            ["Custom Use Rod"] = nil,
+            ["Custom Bait"] = nil,
+        },
+        ["AutoBuy"] = {
+            ["Fully Get Aurora Rod"] = false,
+        },
+        ["Fully Auto All"] = true,
+        ["WebHook"] = {
+            ["Link"] = 'https://discord.com/api/webhooks/1305743894444773436/9VCZphRg4IlHsLR3C3alXwqNt713DYrSGvGN5owN7DmDi3Dm749NdbZeXchBc1bUBMRb',
+            ["Discord Id"] = '',
+            ["Delay Sending"] = 600, 
+            ["Auto Sending"] = false,
+            ["Ping With Discord Id"] = false,
+        },
+    }
+
+
+print("[DEBUG] Script started")
+local success, result = pcall(function()
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/MonkiTeam/Nomal-Project/refs/heads/main/FischKaitun"))()
+end)
+
+if success then
+    print("[DEBUG] Script loaded successfully")
+else
+    warn("[DEBUG] Failed to load script:", result)
+end
