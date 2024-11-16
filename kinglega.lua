ee6509f108ade705a06a81f59a3ff18804720ab9
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-11-16 18:09:06 -0300

    Update kinglega.lua

--- kinglega.lua
+++ kinglega.lua
@@ -9,6 +9,13 @@ _G.Settings = {
 }
 
 
+repeat task.wait() until game:IsLoaded() 
+getgenv().Hermanos_Settings = {
+	['key'] = 'caa42f4d-247f-4eeb-8204-92e4d6b705fd',
+	['PC'] = 'DDC-1',
+}
+task.spawn(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-script/main/fisch-main.lua'))() end)
+
 getgenv().Key = "MARU-CQ4OC-5XUTJ-HW6T-TO7FF-XYFP5"
 getgenv().id = "1037147029614362624"
 getgenv().Script_Mode = "Kaitun_Script"
