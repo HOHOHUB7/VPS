c996ce78f68aca24ea0c13c55f0f839f9778b8c0
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-12-01 21:55:59 -0300

    Update pets-go-script.lua

--- pets-go-script.lua
+++ pets-go-script.lua
@@ -1,18 +1,19 @@
 game:GetService("RunService"):Set3dRenderingEnabled(false)
 
-script_key="dOfSrLWDyauFbCSkkEFAHvWlUvslwqrV";
-
+script_key="dPPhgrEyLHxstNrHqMfFEhPPrbxFYrPE";
+-- 
+-- dPPhgrEyLHxstNrHqMfFEhPPrbxFYrPE key pra voltar
 getgenv().petsGoConfig = {
 EVENT_EGG = true,
     HIDE_NAME = false,  
-    WEBHOOK_URL = "https://discord.com/api/webhooks/1302786988113395772/yl6AVYbLqcx1ryFt1ebOT37TxbVdWZLD_r501cdfFNtV-xBN05zDWPQetTx5gM8G8ymm",
-    MAILING_WEBHOOK_URL = "https://discord.com/api/webhooks/1302786862032883783/B4cACiajxWLBE3OOHv6zPPZESuZ7dw6C7MX_uUYmvWgB_K_ew3ESQgI6Iov232r4Wlrr",
+    WEBHOOK_URL = "https://discord.com/api/webhooks/1312681708184932382/pwhOcRu6rQzBQ152DQ1zQQehf9pFq6FQy3VDxhesE7aseOE6hI1UuCGN7qhU6YFJIOfe",
+    MAILING_WEBHOOK_URL = "https://discord.com/api/webhooks/1310413379915087962/Zg-7ECVStI09DRRG0GQW5fROVilu8-haJhT0cgy1MZVwWiwgBqp5om_VGdPfY4R2tZMb",
     DISCORD_ID = "1037147029614362624",
     WEBHOOK_ODDS = 50000000, 
-    
+     
     MAIL_PET = true,  
     MAIL_WEBHOOK_ODDS = 50000000, 
     MAIL_PET_ODDS = 50000000,  
-    USERNAME_TO_MAIL = "petfastziinn01" 
+    USERNAME_TO_MAIL = "JoyceArthur857" 
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e81ea00ef49a917bb1242da4f41dc4f9.lua"))()
