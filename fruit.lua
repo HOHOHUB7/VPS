c760bfb251280bb3d8f74d777c8d1c2fb7b75801
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-12-10 16:18:02 -0300

    Update fruit.lua

--- fruit.lua
+++ fruit.lua
@@ -9,6 +9,7 @@ FullScreenUI.createParagraph("Username: N/A", 1, 4)
 FullScreenUI.createParagraph("Gems: N/A", 2, 4)
 FullScreenUI.createParagraph("Fruits: N/A", 3, 4)
 FullScreenUI.createParagraph("Place: N/A", 4, 4)
+FullScreenUI.createParagraph("Action: N/A", 5, 4)
 
 -- Atualizar parágrafos depois de algum tempo
 task.delay(5, function()
