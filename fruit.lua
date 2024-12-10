a8d92d07e5f6ea32f7a20b0c12ba6ea99b119fc2
HOHOHUB7 <155407727+HOHOHUB7@users.noreply.github.com>
2024-12-10 17:32:53 -0300

    Update fruit.lua

--- fruit.lua
+++ fruit.lua
@@ -137,7 +137,7 @@ task.spawn(function()
                 game:GetService("ReplicatedStorage").Replicator:InvokeServer(unpack(args))
             end
 
-            if gems > 49 then
+            if D_GEMS > 49 then
                 local theSlot = game:GetService("Players").LocalPlayer.MAIN_DATA.Slot.Value
             
                 spinFruit(theSlot)
